6fdd4a1be13df7ed9cf91582df0851f9981c5f44
Arianna Lima da Silva <arianna.silva@PROVIDER-IT.CORP>
2018-10-29 17:25:28 -0300

    Ajuste nas views.

--- src/main/java/com/appoc/model/Incidente.java
+++ src/main/java/com/appoc/model/Incidente.java
@@ -11,8 +11,6 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import com.appoc.model.enums.TipoEnum;
-
 
 @Entity
 public class Incidente implements Serializable{
